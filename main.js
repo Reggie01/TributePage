8d43ec01e5e7e18c24d92409a93a3d815d840f42
Reggie S <reggie@bgcreatives.com>
2016-10-19 22:27:39 -0400

    Css, images, and js added
    added functionality for timeline visibility on scroll. Css styling of timeline and web page color scheme. Images from wikimedia commons of Octavia Butler added to project.

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿function isElementInViewport( el ) {
+    var rect = el.getBoundingClientRect();
+    return (
+       rect.top > 0 &&
+       rect.left > 0 &&
+       rect.bottom <= ( window.innerHeight || document.documentElement.clientHeight ) &&
+       rect.right <= ( window.innerWidth || document.documentElement.clientWidth )
+    );
+}
+
+var items = document.querySelectorAll( ".timeline li" );
+
+function callBackFunc() {
+  for( var i = 0; i < items.length; i++ ) {
+    if( isElementInViewport( items[i] ) ) {
+      items[i].classList.add( "in-view" );
+    }
+  }
+}
+
+window.addEventListener( "load", callBackFunc );
+window.addEventListener( "scroll", callBackFunc );
